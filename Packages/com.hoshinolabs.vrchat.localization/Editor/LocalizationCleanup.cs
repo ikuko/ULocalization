4ae01c543563f4f292ba4d0aeb32e3a8a3b4ca00
ikuko <magi.ikuko@gmail.com>
2024-03-12 03:45:51 +0900

    refactor: 表記ミスを修正

--- Packages/com.hoshinolabs.vrchat.localization/Editor/LocalizationCleanup.cs
+++ Packages/com.hoshinolabs.vrchat.localization/Editor/LocalizationCleanup.cs
@@ -5,11 +5,10 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
-using UnityEngine.Localization.Components;
 using UnityEngine.SceneManagement;
 
 namespace HoshinoLabs.Localization {
-    internal sealed class SardinjectCleanup : IProcessSceneWithReport {
+    internal sealed class LocalizationCleanup : IProcessSceneWithReport {
         public int callbackOrder => 100;
 
         public void OnProcessScene(Scene scene, BuildReport report) {
